36ae60880ef8a8eed4c298ccaf5e7b6c25839fd1
Sandesh Kumar <sandesh.kuamr@gmail.com>
2019-02-25 12:20:16 +0500

    created new environment for mobile division LM system

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,9 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
+//------------- environment for all divisions firebase project
+/*
 export const environment = {
   production: false,
   firebase: {
@@ -11,6 +14,20 @@ export const environment = {
     projectId: "line-management-system",
     storageBucket: "line-management-system.appspot.com",
     messagingSenderId: "585047453826"
+  }
+};
+*/
+
+//------------- environment for mobile division firebase project
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAGEJPXiMBf1EvospTTd8QNnVMo6G9yfO4",
+    authDomain: "mobile-division-lm-system.firebaseapp.com",
+    databaseURL: "https://mobile-division-lm-system.firebaseio.com",
+    projectId: "mobile-division-lm-system",
+    storageBucket: "mobile-division-lm-system.appspot.com",
+    messagingSenderId: "824300028661"
   }
 };
 
